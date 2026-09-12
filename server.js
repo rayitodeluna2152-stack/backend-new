b86fdb2122882a0ae23daff22803cce5ca6ecfc1
rayitodeluna2152-stack <rayitodeluna2152@gmail.com>
2026-09-12 11:48:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ async function generarRespuesta(systemPrompt, mensaje) {
     try {
         console.log("🧠 Mensaje enviado a Groq:", mensaje); // ⭐ LOG
         const completion = await groq.chat.completions.create({
-           model: "llama3-70b-versatile",
+        model: "llama3-70b",
             messages: [
                 { role: "system", content: systemPrompt },
                 { role: "user", content: mensaje }

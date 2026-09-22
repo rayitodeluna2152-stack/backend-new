8e1aa2142e378951182e16fb80d214de2b828d59
rayitodeluna2152-stack <rayitodeluna2152@gmail.com>
2026-09-22 15:02:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const OpenAI = require("openai");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
